9158d4da25f70bd5bb880c18207609eef707224f
omprakashch3 <omprakashchautala3@gmail.com>
2020-10-04 18:47:34 +0530

    modified Header

--- hello/src/components/Header.vue
+++ hello/src/components/Header.vue
@@ -1,17 +1,19 @@
 <template>
   <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="container">
+      <a class="navbar-brand" href="#">Navbar</a>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active"></li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/about" class="nav-link">About</router-link>
-        </li>
-      </ul>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active"></li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
@@ -24,18 +26,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
